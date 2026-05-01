ab4ed9449c1b2410acd63d189ae9f98f26722d0e
valmarosaalves-cmyk <valmarosaalves@gmail.com>
2026-05-01 14:48:26 -0300

    Update GameTransition.hx

--- source/backend/game/GameTransition.hx
+++ source/backend/game/GameTransition.hx
@@ -1,28 +1,22 @@
 package backend.game;
 
+import flixel.FlxG;
 import flixel.FlxSprite;
-import flixel.util.FlxGradient;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.util.FlxColor;
 import flixel.tweens.FlxTween;
+import flixel.tweens.FlxEase;
 import backend.game.MusicBeatData.MusicBeatSubState;
-
-/*
-	Transition between states.
-
-	Usage: When changing between states, you can choose which transition will play.
-	Main.switchState(new states.menu.MainMenuState(), "base");
-*/
 
 class GameTransition extends MusicBeatSubState
 {	
 	var fadeOut:Bool = false;
 	var transition:String = 'funkin';
 	
-	// Callback at the end of the transition
 	public var finishCallback:Void->Void;
 
-	// Sprites used in transitions
+	var blocks:FlxTypedGroup<FlxSprite>;
 	var sprBlack:FlxSprite;
-	var sprGrad:FlxSprite;
 	
 	public function new(fadeOut:Bool = true, transition:String = "funkin")
 	{
@@ -32,31 +26,40 @@ class GameTransition extends MusicBeatSubState
 
 		switch(transition) {
 			case 'funkin':
-				sprBlack = new FlxSprite().makeGraphic(FlxG.width * 2, FlxG.height * 2, 0xFF000000);
-				sprBlack.screenCenter(X);
-				add(sprBlack);
-				
-				sprGrad = FlxGradient.createGradientFlxSprite(FlxG.width, Math.floor(FlxG.height / 2), [0xFF000000, 0x00], 1, 90);
-				sprGrad.screenCenter(X);
-				sprGrad.flipY = fadeOut;
-				add(sprGrad);
-				
-				var yPos:Array<Float> = [
-					-sprBlack.height - sprGrad.height - 40,	// upper
-					FlxG.height / 2 - sprBlack.height / 2, 	// middle
-					FlxG.height + sprGrad.height + 40		// bottom
-				];
-				var curY:Int = (fadeOut ? 1 : 0);
-				
-				sprBlack.y = yPos[curY];
-				updateGradPos();
+				blocks = new FlxTypedGroup<FlxSprite>();
+				add(blocks);
 
-				FlxTween.tween(sprBlack, {y: yPos[curY + 1]}, fadeOut ? 0.6 : 0.8, {
-					onComplete: function(twn:FlxTween)
-					{
-						endTransition();
+				var blockSize:Int = 100;
+				var cols:Int = Math.ceil(FlxG.width / blockSize);
+				var rows:Int = Math.ceil(FlxG.height / blockSize);
+				
+				for (y in 0...rows) {
+					for (x in 0...cols) {
+						var block = new FlxSprite(x * blockSize, y * blockSize).makeGraphic(blockSize, blockSize, FlxColor.BLACK);
+						block.antialiasing = false;
+						
+						if (fadeOut) {
+							block.scale.set(1, 1);
+						} else {
+							block.scale.set(0, 0);
+						}
+						
+						blocks.add(block);
+
+						var delay:Float = (x + y) * 0.04; 
+						
+						FlxTween.tween(block.scale, {x: (fadeOut ? 0 : 1), y: (fadeOut ? 0 : 1)}, 0.4, {
+							ease: FlxEase.cubeInOut,
+							startDelay: delay,
+							onComplete: function(twn:FlxTween) {
+								if (x == cols - 1 && y == rows - 1) {
+									endTransition();
+								}
+							}
+						});
 					}
-				});
+				}
+
 			default:
 				sprBlack = new FlxSprite().makeGraphic(FlxG.width * 2, FlxG.height * 2, 0xFF000000);
 				sprBlack.screenCenter();
@@ -80,21 +83,9 @@ class GameTransition extends MusicBeatSubState
 			close();
 	}
 	
-	function updateGradPos():Void {
-		sprGrad.y = sprBlack.y + (fadeOut ? -sprGrad.height : sprBlack.height);
-	}
-	
 	override function update(elapsed:Float)
 	{
 		super.update(elapsed);
-
 		this.cameras = [FlxG.cameras.list[FlxG.cameras.list.length - 1]];
-
-		switch(transition) {
-			case 'funkin':
-				updateGradPos();
-			default:
-				// do nothing
-		}
 	}
 }
